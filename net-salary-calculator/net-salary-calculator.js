ea871e6457e3f1d2c91e0696cd38096b7e4521ad
ArshavineRoy <arshavinewaema@gmail.com>
2023-06-19 14:32:57 +0300

    Changed a few keyword tags: gross pay => basic salary, other insurance => insurance premiums

--- net-salary-calculator/net-salary-calculator.js
+++ net-salary-calculator/net-salary-calculator.js
@@ -6,16 +6,16 @@
 
 
 
-let grossPay = prompt("What is your monthly gross salary? ");        // prompt the user for input. Almost all inputs treated similarly.
-grossPay = parseInt(grossPay.replace(/,/g, ""), 10);                 // parseInt () and .replace () methods used to convert input into an integer and remove all commas, if present
-console.log(`Gross Salary: ${grossPay.toLocaleString()}`);           // .toLocaleString() method used to log comma separated values e.g., 1000 = 1,000
+let basicSalary = prompt("What is your monthly basic salary? ");        // prompt the user for input. Almost all inputs treated similarly.
+basicSalary = parseInt(basicSalary.replace(/,/g, ""), 10);                 // parseInt () and .replace () methods used to convert input into an integer and remove all commas, if present
+console.log(`Basic Salary: ${basicSalary.toLocaleString()}`);           // .toLocaleString() method used to log comma separated values e.g., 1000 = 1,000
 
 let benefits = prompt("What are your monthly benefits/allowances? ");
 benefits = parseInt(benefits.replace(/,/g, ""), 10);
 console.log(`Benefits/allowances: ${benefits.toLocaleString()}`);
 
-let otherInsurance = prompt("Apart from NHIF, any other insurance relief received? Enter total amount, or 0 if none:"); // Ask user to enter any other insurance they have apart from NHIF for tax relief calculations.
-otherInsurance = parseInt(otherInsurance);
+let insurancePremiums = prompt("Do you have any insurance premiums? Enter total amount, or 0 if none:"); // Ask user to enter any insurance premiums they have.
+insurancePremiums = parseInt(insurancePremiums);
 
 const personalRelief = 2400;
 
@@ -23,18 +23,18 @@ const personalRelief = 2400;
 // 1. NSSF
 // A function for calculating NSSF deductions. All percentages expressed as floats i.e., 6% = 0.06.
 
-function NSSFDeductions (grossPay){
+function NSSFDeductions (basicSalary){
     let NSSFRate = prompt("What are your NSSF rates? \n 1. New Rates \n 2. Old Rates")
     if (NSSFRate === "1"){
-        if (grossPay <= 6000){                                // Tier 1: employee contributes 6% of amount if gross pay is below 6000. Tier 1 default value = 360 (6000 * 0.06)
-            return (grossPay * 0.06);
-        }else if (grossPay > 6000 && grossPay <=18000){       // Tier 2: employee contributes  6% of any amount between 6,001 - 18,000, added to Tier 1's contribution
-            return 360 + ((grossPay - 6000) * 0.06);
+        if (basicSalary <= 6000){                                // Tier 1: employee contributes 6% of amount if basic pay is below 6000. Tier 1 default value = 360 (6000 * 0.06)
+            return (basicSalary * 0.06);
+        }else if (basicSalary > 6000 && basicSalary <=18000){       // Tier 2: employee contributes  6% of any amount between 6,001 - 18,000, added to Tier 1's contribution
+            return 360 + ((basicSalary - 6000) * 0.06);
         }else{
             return 1080;                                      // Anything above 18000; Tier 1 default: 360 + Tier 2 default: 720 = 1080
         }
     }else if (NSSFRate === "2"){
-        let NSSFcontribution = (grossPay * 0.05)              // monthly gross pay subject to 5% deductions per old rates, capped at 200.
+        let NSSFcontribution = (basicSalary * 0.05)              // monthly basic pay subject to 5% deductions per old rates, capped at 200.
         if (NSSFcontribution <= 200){                         // Contribution shouldn't exceed 200.
             return NSSFcontribution;
         }else{
@@ -45,20 +45,20 @@ function NSSFDeductions (grossPay){
     }
 }
 
-let yourNSSFDeductions = NSSFDeductions (grossPay);          // assigning function to a variable for easier reference
+let yourNSSFDeductions = NSSFDeductions (basicSalary);          // assigning function to a variable for easier reference
 console.log(`NSSF Deductions: ${yourNSSFDeductions.toLocaleString()}`);
 
 
 
 // 2. PAYE
 
-// A function to for calculating PAYE from gross pay
+// A function to for calculating PAYE from basic pay
 // New income tax rates were introduced in Kenya on January 1, 2022. The rates are progressive, meaning that the higher your income, the higher your tax rate. 
 // A personal relief of Ksh2,400 per month applies, which is subtracted from your tax payable.
-// PAYE is calculated by first determining your taxable income. This is done by subtracting your pension contributions from your gross income. 
+// PAYE is calculated by first determining your taxable income. This is done by subtracting your pension contributions from your basic income. 
 // Your taxable income is then taxed using the progressive income tax rates.
 
-let taxableIncome = ((grossPay + benefits) - yourNSSFDeductions);
+let taxableIncome = ((basicSalary + benefits) - yourNSSFDeductions);
 console.log(`Taxable Income: ${taxableIncome.toLocaleString()}`);
 
 function PAYEBeforeRelief(taxableIncome){
@@ -78,62 +78,62 @@ let yourPAYEBeforeRelief = PAYEBeforeRelief(taxableIncome);
 
 // 3. NHIF
 
-// A function for calculating NHIF contributions from gross pay
+// A function for calculating NHIF contributions from basic pay
 
-function NHIFContributions(grossPay) {
+function NHIFContributions(basicSalary) {
     let contribution;
 
     // A switch statement based on the NHIF contributions Chart 
     switch (true) {
-      case (grossPay <= 5999):
+      case (basicSalary <= 5999):
         contribution = 150;
         break;
-      case (grossPay >= 6000 && grossPay <= 7999):
+      case (basicSalary >= 6000 && basicSalary <= 7999):
         contribution = 300;
         break;
-      case (grossPay >= 8000 && grossPay <= 11999):
+      case (basicSalary >= 8000 && basicSalary <= 11999):
         contribution = 400;
         break;
-      case (grossPay >= 12000 && grossPay <= 14999):
+      case (basicSalary >= 12000 && basicSalary <= 14999):
         contribution = 500;
         break;
-      case (grossPay >= 15000 && grossPay <= 19999):
+      case (basicSalary >= 15000 && basicSalary <= 19999):
         contribution = 600;
         break;
-      case (grossPay >= 20000 && grossPay <= 24999):
+      case (basicSalary >= 20000 && basicSalary <= 24999):
         contribution = 750;
         break;
-      case (grossPay >= 25000 && grossPay <= 29999):
+      case (basicSalary >= 25000 && basicSalary <= 29999):
         contribution = 850;
         break;
-      case (grossPay >= 30000 && grossPay <= 34999):
+      case (basicSalary >= 30000 && basicSalary <= 34999):
         contribution = 900;
         break;
-      case (grossPay >= 35000 && grossPay <= 39999):
+      case (basicSalary >= 35000 && basicSalary <= 39999):
         contribution = 950;
         break;
-      case (grossPay >= 40000 && grossPay <= 44999):
+      case (basicSalary >= 40000 && basicSalary <= 44999):
         contribution = 1000;
         break;
-      case (grossPay >= 45000 && grossPay <= 49999):
+      case (basicSalary >= 45000 && basicSalary <= 49999):
         contribution = 1100;
         break;
-      case (grossPay >= 50000 && grossPay <= 59999):
+      case (basicSalary >= 50000 && basicSalary <= 59999):
         contribution = 1200;
         break;
-      case (grossPay >= 60000 && grossPay <= 69999):
+      case (basicSalary >= 60000 && basicSalary <= 69999):
         contribution = 1300;
         break;
-      case (grossPay >= 70000 && grossPay <= 79999):
+      case (basicSalary >= 70000 && basicSalary <= 79999):
         contribution = 1400;
         break;
-      case (grossPay >= 80000 && grossPay <= 89999):
+      case (basicSalary >= 80000 && basicSalary <= 89999):
         contribution = 1500;
         break;
-      case (grossPay >= 90000 && grossPay <= 99999):
+      case (basicSalary >= 90000 && basicSalary <= 99999):
         contribution = 1600;
         break;
-      case (grossPay >= 100000):
+      case (basicSalary >= 100000):
         contribution = 1700;
         break;
       default:
@@ -144,20 +144,20 @@ function NHIFContributions(grossPay) {
 }
 
 
-let yourNHIFContributions = NHIFContributions(grossPay);
+let yourNHIFContributions = NHIFContributions(basicSalary);
 
 console.log(`NHIF Contributions: ${yourNHIFContributions.toLocaleString()}`);
 
 
 // Tax Relief calculations
-// monthly NHIF contributions qualify for 15% insurance relief, capped at 5,000. 
+// Per KRA, Insurance Relief = 15% (Insurance Premiums + NHIF Contributions) but shall not exceed Kshs. 5,000.00 per month or Kshs. 60,000.00 per year.
 
-function insuranceRelief(yourNHIFContributions, otherInsurance) {
+function insuranceRelief(yourNHIFContributions, insurancePremiums) {
     let totalInsuranceRelief;
 
-    if (otherInsurance !== 0) {                                                 // an if statement for user input that is not 0; i.e., for a person who has another form of insurance
-        totalInsuranceRelief = yourNHIFContributions * 0.15 + otherInsurance;
-        console.log(`Other Insurance: ${otherInsurance}`);
+    if (insurancePremiums !== 0) {                                                 // an if statement for user input that is not 0; i.e., for a person who has another form of insurance
+        totalInsuranceRelief = yourNHIFContributions * 0.15 + insurancePremiums;
+        console.log(`Insurance Premiums: ${insurancePremiums}`);
     } else {
         totalInsuranceRelief = yourNHIFContributions * 0.15;                    // if a user doesn't have another form of insurance
     }
@@ -167,7 +167,7 @@ function insuranceRelief(yourNHIFContributions, otherInsurance) {
 }
 
 
-let yourInsuranceRelief = insuranceRelief(yourNHIFContributions, otherInsurance);
+let yourInsuranceRelief = insuranceRelief(yourNHIFContributions, insurancePremiums);
 
 console.log(`Insurance Relief: ${yourInsuranceRelief.toLocaleString()}`);
 
@@ -177,6 +177,6 @@ let PAYEDue = Math.round((yourPAYEBeforeRelief - (yourInsuranceRelief + personal
 console.log(`PAYE Due: ${PAYEDue.toLocaleString()}`);
 
 
-// Net monthly pay = gross pay - total deductions (NSSF, NHIF, PAYE)
+// Net monthly pay = basic pay - total deductions (NSSF, NHIF, PAYE)
 
-console.log(`Net Pay: ${(grossPay - PAYEDue).toLocaleString()}`)
+console.log(`Net Pay: ${(basicSalary - PAYEDue).toLocaleString()}`)
